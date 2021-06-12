55ec353ca448fa17817e76586880c63494d29215
Lars <larsoyvind28@gmail.com>
2021-06-13 01:58:45 +0200

    update server.js

--- srv/server.js
+++ srv/server.js
@@ -4,4 +4,5 @@ const port = process.env.PORT || 5000;
 
 express()
     .use(express.static(path.join(__dirname, 'dist')))
+    .get('/', (req, res) => res.render('index'))
     .listen(port, () => console.log(`Listening on ${ port }`))

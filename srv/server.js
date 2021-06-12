56fe91272c21e017cc282f589a25f57a9c7351f1
Lars <larsoyvind28@gmail.com>
2021-06-13 01:45:54 +0200

    add express

--- srv/server.js
+++ srv/server.js
@@ -3,5 +3,5 @@ const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 app.listen(port);

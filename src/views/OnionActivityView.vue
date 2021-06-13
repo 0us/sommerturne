4ba7864babab8296fbeef1a5ae49ef72998a8d22
markusranda <randamarkus.95@gmail.com>
2021-06-13 15:23:26 +0200

    Added sound effect and text to kill streaks.

--- src/views/OnionActivityView.vue
+++ src/views/OnionActivityView.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="bowling-container">
+        <CrazyText :msg="currentCrazyText" level="2" />
         <div v-if="showBowling" class="bowling-bane">
             <img
                 ref="bowlingBall"
@@ -15,7 +16,12 @@
             />
             <Explosion class="bowling-kjegle" />
         </div>
-        <TurboButton title="Bowl" :action="bowl" :disabled="showBowling" />
+        <div class="bottom-container">
+            <div class="turbo-counter text-white">
+                {{ killCount }}
+            </div>
+            <TurboButton title="Bowl" :action="bowl" :disabled="showBowling" />
+        </div>
     </div>
 </template>
 
@@ -23,18 +29,39 @@
 import Explosion from '@/components/Explosion.vue'
 import { eventHub } from '../main'
 import TurboButton from '@/components/TurboButton.vue'
+import CrazyText from '@/components/CrazyText.vue'
 
 const ANIMATION_DURATION = 500
 const IMG_SIZE = 100
 export default {
-    components: { Explosion, TurboButton },
+    components: { Explosion, TurboButton, CrazyText },
     data() {
         return {
             showBowling: false,
+            showCrazyText: false,
+            currentCrazyText: '',
             kjegleIsKill: false,
             kjegleStartPos: 0,
+            killCount: 0,
+            fxSounds: {
+                1: new Audio('fx/head_shot.mp3'),
+                2: new Audio('fx/double_kill.mp3'),
+                3: new Audio('fx/triple_kill.mp3'),
+                4: new Audio('fx/multi_kill.mp3'),
+                5: new Audio('fx/monster_kill.mp3'),
+                10: new Audio('fx/god_like.mp3'),
+            },
+            crazyTexts: {
+                1: 'Head Shot!',
+                2: 'Double Kill!',
+                3: 'Triple Kill!',
+                4: 'Multi Kill!',
+                5: 'Monster Kill!',
+                10: 'God Like!',
+            },
         }
     },
+    mounted() {},
     methods: {
         explode() {
             eventHub.$emit('explosion')
@@ -70,6 +97,19 @@ export default {
         cleanupBowling(intervalId) {
             this.showBowling = false
             this.kjegleIsKill = false
+            this.killCount++
+
+            // Show crazyText
+            this.showCrazyText = true
+            this.currentCrazyText = this.crazyTexts[this.killCount]
+            setTimeout(() => {
+                this.showCrazyText = false
+            }, 500)
+
+            // If killstreak is achieved, play sound
+            const fx = this.fxSounds[this.killCount]
+            fx.play()
+
             clearInterval(intervalId)
         },
     },
@@ -82,6 +122,21 @@ export default {
     flex-direction: column;
     align-items: center;
     height: 100%;
+}
+
+.bottom-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 2rem;
+    margin-top: auto;
+}
+
+.turbo-counter {
+    left: 0;
+    position: absolute;
+    margin-left: 2rem;
+    font-size: 5rem;
 }
 
 .bowling-bane {

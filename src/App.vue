8ba1650ef63bd3cbe069af9c05c2e8fed4b5a9c2
markusranda <randamarkus.95@gmail.com>
2021-06-13 12:26:24 +0200

    moved animations to seperate file

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-row">
             <router-link to="/">
                 <img
-                    class="p-2 turbo-link"
+                    class="p-2 spin"
                     src="house.png"
                     alt="house"
                     width="50px"
@@ -12,7 +12,7 @@
 
             <router-link to="onion_activity">
                 <img
-                    class="p-2 turbo-link"
+                    class="p-2 spin"
                     src="bowling.png"
                     alt="bowling"
                     width="50px"
@@ -38,55 +38,11 @@ export default Vue.extend({
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@800&display=swap');
+@import './styles/main.css';
 
 #app {
     font-family: Inter, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-}
-
-.turbo-link {
-    -webkit-animation-name: spin;
-    -webkit-animation-duration: 1000ms;
-    -webkit-animation-iteration-count: infinite;
-    -webkit-animation-timing-function: linear;
-    -moz-animation-name: spin;
-    -moz-animation-duration: 1000ms;
-    -moz-animation-iteration-count: infinite;
-    -moz-animation-timing-function: linear;
-    -ms-animation-name: spin;
-    -ms-animation-duration: 1000ms;
-    -ms-animation-iteration-count: infinite;
-    -ms-animation-timing-function: linear;
-
-    animation-name: spin;
-    animation-duration: 1000ms;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
-}
-
-@-moz-keyframes spin {
-    from {
-        -moz-transform: rotate(0deg);
-    }
-    to {
-        -moz-transform: rotate(360deg);
-    }
-}
-@-webkit-keyframes spin {
-    from {
-        -webkit-transform: rotate(0deg);
-    }
-    to {
-        -webkit-transform: rotate(360deg);
-    }
-}
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
 }
 </style>

2811c81e445d36e377c9194a148e2960efae89f1
Lars <larsoyvind28@gmail.com>
2021-06-13 16:51:57 +0200

    add chill sun

--- src/views/ChillActivityView.vue
+++ src/views/ChillActivityView.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="flex flex-col items-center ">
 
+    <img src="sun.png" v-if="showSun" class="spin absolute top-12 right-12 w-40">
+
     <div class="bottom-container">
       <div>
         <img src="palme.webp" class="w-auto flex-shrink h-64">
       </div>
-      <TurboButton title="Chill" :action="randomChillFx"></TurboButton>
+      <TurboButton
+          title="Chill"
+          :action="randomChillFx"
+      ></TurboButton>
       <div>
         <img src="palme.webp" class="w-auto flex-shrink  h-64">
       </div>
@@ -21,19 +26,33 @@ export default {
   components: {TurboButton},
   data() {
     return {
+      buttonClicked: false,
+      showSun: false,
       currentAudio: null,
       coolAudio: [
         new Audio("fx/chill_fx/beer1.wav"),
         new Audio("fx/chill_fx/beer2.wav"),
-        new Audio("fx/chill_fx/seagull.wav")
+        new Audio("fx/chill_fx/seagull.wav"),
+        new Audio("fx/chill_fx/slurp.wav"),
+        new Audio("fx/chill_fx/ducks.m4a"),
+        new Audio("fx/chill_fx/splash.wav")
       ]
     }
   },
+  mounted() {
+    this.coolAudio.forEach((audio) => {
+      audio.addEventListener("ended", () => {
+        this.showSun = false
+      })
+    })
+  },
   methods: {
     randomChillFx() {
+
       if (this.currentAudio != null) {
         this.currentAudio.pause()
         this.currentAudio.currentTime = 0
+        this.showSun = false
       }
 
       this.currentAudio = this.coolAudio[
@@ -41,6 +60,7 @@ export default {
           ]
 
       if (this.currentAudio != null) {
+        this.showSun = true
         this.currentAudio.play()
       }
 

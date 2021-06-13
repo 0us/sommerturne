959a91f175920f9ddc84f2ccb42d9ada89ea314c
Lars <larsoyvind28@gmail.com>
2021-06-13 02:11:24 +0200

    add logging to server.ts

--- srv/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 5000;
-
-express()
-    .use(express.static(path.join(__dirname, 'dist')))
-    .get('/', (req, res) => res.render('index'))
-    .listen(port, () => console.log(`Listening on ${ port }`))

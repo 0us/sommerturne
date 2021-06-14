6111932eed90406415079840e5816b5e0e8361f3
Frode Pedersen <frodeluren@gmail.com>
2021-06-14 21:55:01 +0200

    Initializes socket in backend.

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const app = express();
 const port = process.env.PORT || 5000;
 
+io.on('connection', () => {
+    console.log("Something connected!");
+});
+
 // @ts-ignore
-app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(
-    history({
-        disableDotRule: true,
-        verbose: true,
-    })
-);
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.use(serveStatic(path.join(__dirname, 'dist')))
+    .use(
+        history({
+            disableDotRule: true,
+            verbose: true,
+        })
+    )
+    .listen(port, () => console.log(`Listening on ${port}`));

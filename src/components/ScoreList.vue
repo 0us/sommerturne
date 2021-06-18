e79ab220dd5bbd6eb462b7051710af7b184a68b9
markusranda <randamarkus.95@gmail.com>
2021-06-18 23:17:11 +0200

    styleupgrade of the score

--- src/components/ScoreList.vue
+++ src/components/ScoreList.vue
@@ -3,6 +3,8 @@
         <ul
             class="
                 p-5
+                w-52
+                overflow-ellipsis
                 bg-gradient-to-r
                 from-purple-500
                 to-green-700
@@ -34,5 +36,6 @@ export default {
     margin-left: 2rem;
     display: flex;
     animation: bounce-in 5s infinite;
+    filter: drop-shadow(0 50px 50px rgba(0, 0, 0, 0.5));
 }
 </style>
